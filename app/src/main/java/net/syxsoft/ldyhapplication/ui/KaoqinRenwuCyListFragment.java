eedf9805eaad57dca25bce1967cad37e85938fba
huzhimin <646708090@qq.com>
2018-03-15 12:55:05 +0800

    任务管理 相关界面

---
+++ app/src/main/java/net/syxsoft/ldyhapplication/ui/KaoqinRenwuCyListFragment.java
@@ -0,0 +1,59 @@
+package net.syxsoft.ldyhapplication.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.syxsoft.ldyhapplication.R;
+
+import java.util.List;
+
+import butterknife.OnClick;
+
+
+public class KaoqinRenwuCyListFragment extends BaseFragment {
+
+
+    @OnClick(R.id.renwu_cy_detail_show)
+    public void onNewRenwuBtnClicked(){getHoldingActivity().pushFragment(new KaoqinRenwuDetailsFragment());}
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_kaoqing_renwu_cy_list;
+    }
+
+    @Override
+    protected void initActionBar(Toolbar actionBar) {
+        super.initActionBar(actionBar);
+
+        //设置标题
+        TextView textView = actionBar.findViewById(R.id.toolbar_title);
+        textView.setText("我参与的");
+
+        //启用返回导航
+        actionBar.setNavigationIcon(R.mipmap.title_bar_back);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //去掉底部导航
+        BottomNavigationView navigation= getHoldingActivity().findViewById(R.id.navigation);
+        navigation.setVisibility(View.GONE);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
