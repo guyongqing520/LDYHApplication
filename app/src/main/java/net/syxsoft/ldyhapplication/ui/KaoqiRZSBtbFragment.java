bd61d84c1308492401ae4964aed4587bd641f062
guyongqing520 <371395204@qq.com>
2018-03-22 21:08:22 +0800

    更新全部页面效果

--- app/src/main/java/net/syxsoft/ldyhapplication/ui/KaoqiRZSBtbFragment.java
+++ app/src/main/java/net/syxsoft/ldyhapplication/ui/KaoqiRZSBtbFragment.java
@@ -25,16 +25,21 @@ import net.syxsoft.ldyhapplication.R;
 import butterknife.BindView;
 import butterknife.OnClick;
 import okhttp3.Call;
+import okhttp3.Response;
 
 import net.syxsoft.ldyhapplication.bean.AttendenceBean;
 import net.syxsoft.ldyhapplication.callback.GsonObjectCallback;
+import net.syxsoft.ldyhapplication.callback.LoadCallBack;
 import net.syxsoft.ldyhapplication.model.rztb_bglx_model;
 import net.syxsoft.ldyhapplication.bean.RzsbRztbBean;
 import net.syxsoft.ldyhapplication.utils.MD5Utils;
 import net.syxsoft.ldyhapplication.utils.OkHttp3Utils;
+import net.syxsoft.ldyhapplication.utils.OkHttpManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,6 +48,7 @@ public class KaoqiRZSBtbFragment extends BaseFragment {
 
     private OptionsPickerView  optionsPickerView;
     private ArrayList<rztb_bglx_model> Rztb_bglx_list=new ArrayList<>();
+    private String userId;
 
     @BindView(R.id.rzsb_gznr)
     EditText rzsb_gznr;
@@ -97,47 +103,42 @@ public class KaoqiRZSBtbFragment extends BaseFragment {
             Toast.makeText(getHoldingActivity(), "没有网络连接，请稍后重试", Toast.LENGTH_SHORT).show();
             return false;
         }
-        String jsonParams="{\"personId\":\"11111\",\"type\":\"1\",\"address\":\"四川成都\",\"taskId\":\"12\",\"content\":\"这里是日志的内容部分\" \"}";
 
-        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("personId", userId);
+        params.put("type", "1");
+        params.put("address", "四川成都");
+        params.put("taskId", "12");
+        params.put("content", "这里是日志的内容部分");
 
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.setMessage("加载中...");
-        progressDialog.show();
-
-        if (item.getItemId()==R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
             //提交信息
-            try {
-                OkHttp3Utils.getInstance().doPostJson(getRootApiUrl() + "/api/workdaily/write/",jsonParams,
-                        new GsonObjectCallback<RzsbRztbBean>() {
 
-                            @Override
-                            public void onSuccess(RzsbRztbBean rzsbRztbBean) {
-                                progressDialog.dismiss();
+            OkHttpManager.getInstance().postRequest(getRootApiUrl() + "/api/workdaily/write/",
+                    new LoadCallBack<RzsbRztbBean>(getContext()) {
 
-                                if (rzsbRztbBean.getRequestCode() != 200) {
-                                    progressDialog.dismiss();
-                                    Toast.makeText(getHoldingActivity(), "网络连接失败，请稍后重试", Toast.LENGTH_SHORT).show();
-                                }
+                        @Override
+                        public void onSuccess(Call call, Response response, RzsbRztbBean rzsbRztbBean) {
+                            if (rzsbRztbBean.getRequestCode() != 200) {
+                                Toast.makeText(getContext(), "提交失败，请稍后重试", Toast.LENGTH_SHORT).show();
                             }
+                        }
 
-                            @Override
-                            public void onFailed(Call call, IOException e) {
-                                progressDialog.dismiss();
-                                Toast.makeText(getHoldingActivity(), "网络连接失败，请稍后重试", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            } catch (Exception ex) {
-                progressDialog.dismiss();
-                Toast.makeText(getHoldingActivity(), "网络连接失败，请稍后重试", Toast.LENGTH_SHORT).show();
-            }
-            }
+                        @Override
+                        public void onEror(Call call, int statusCode, Exception e) {
+
+                        }
+                    }, params);
+
+        }
         return super.onOptionsItemSelected(item);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        userId=getUserId();
 
         //去掉底部导航
         BottomNavigationView navigation= getHoldingActivity().findViewById(R.id.navigation);
